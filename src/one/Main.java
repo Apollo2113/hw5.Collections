eab2f6bbbb092b97d28ee7beef22ae7c8dee8c40
Apollo2113 <petrenkoroman1@gmail.com>
2023-03-31 15:40:14 +0300

    changed class Main

--- src/one/Main.java
+++ src/one/Main.java
@@ -1,7 +1,18 @@
 package one;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<String> catalog = new ArrayList<>();
+        catalog.add("apple");
+        catalog.add("banana");
+        catalog.add("orange");
+        catalog.add("banana");
+        catalog.add("kiwi");
+        catalog.add("banana");
+
+        String element = "banana";
+
+        int occurrenceCount = OccurrenceCounter.countOccurrence(catalog, element);
+        System.out.println("The occurrence count of " + element + " in the catalog is: " + occurrenceCount);
     }
 }
